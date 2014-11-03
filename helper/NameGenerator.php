9883592ec27a8ed7afc6ce3cca9ec4703ce31cd3
Joel Bout <joel@taotesting.com>
2014-11-03 14:24:21 +0100

    Added word sources

--- helper/NameGenerator.php
+++ helper/NameGenerator.php
@@ -22,23 +22,44 @@
  */
 namespace oat\taoDevTools\helper;
 
+/**
+ * Name generator to generate searchable names
+ * 
+ * Based on:
+ *  http://www.englishbanana.com/ (public domain) 
+ *  http://study4ielts.wikispaces.com/ (Creative Commons Attribution Share-Alike 3.0)
+ * 
+ * @author Joel Bout <joel@taotesting.com>
+ *
+ */
 class NameGenerator
 {
-    private static $nouns = array("adventure","amazement","anger","anxiety","apprehension","artistry","arrogance","awe","beauty","belief","bravery","brutality","calm","cactus","chaos","charity","childhood","clarity","coldness","comfort","communication","compassion","confidence","contentment",
+    private static $nouns = array(
+        "adventure","amazement","anger","anxiety","apprehension","artistry","arrogance","awe","beauty","belief","bravery","brutality","calm","cactus","chaos","charity","childhood","clarity","coldness","comfort","communication","compassion","confidence","contentment",
         "courage","crime","curiosity","death","deceit","dedication","defeat","delight","democracy","despair","determination","dexterity","dictatorship","disappointment","disbelief","disquiet","disturbance","education","ego","elegance","energy","enhancement","enthusiasm","envy","evil","excitement","failure",
         "faith","faithfulness","faithlessness","fascination","favouritism","fear","forgiveness","fragility","frailty","freedom","friendship","generosity","goodness","gossip","grace","grief","happiness","hate","hatred","hearsay","helpfulness","helplessness","homelessness","honesty","honour","hope","humility",
         "humour","hurt","idea","idiosyncrasy","imagination","impression","improvement","infatuation","inflation","insanity","intelligence","jealousy","joy","justice","kindness","knowledge","laughter","law","liberty","life","loss","love","loyalty","luck","luxury","man","maturity","memory","mercy","motivation",
         "movement","music","need","omen","opinion","opportunism","opportunity","pain","patience","peace","peculiarity","perseverance","pleasure","poverty","power","pride","principle","reality","redemption","refreshment","relaxation","relief","restoration","riches","romance","rumour","sacrifice","sadness",
         "sanity","satisfaction","self-control","sensitivity","service","shock","silliness","skill","slavery","sleep","sophistication","sorrow","sparkle","speculation","speed","strength","strictness","stupidity","submission","success","surprise","sympathy","talent","thrill","tiredness","tolerance","trust",
-        "uncertainty","unemployment","unreality","victory","wariness","warmth","weakness","wealth","weariness","wisdom","wit","worry");
+        "uncertainty","unemployment","unreality","victory","wariness","warmth","weakness","wealth","weariness","wisdom","wit","worry"
+    );
     
-    private static $adjectives = array("different","used","important","every","large","available","popular","able","basic","known","various","difficult","several","united","historical","hot","useful","mental","scared","additional","emotional","old","political","similar","healthy","financial","medical",
-        "traditional","federal","entire","strong","actual","significant","successful","electrical","expensive","pregnant","intelligent","interesting","poor","happy","responsible","cute","helpful","recent","willing","nice","wonderful","impossible","serious","huge","rare","technical","typical","competitive",
-        "critical","electronic","immediate","aware","educational","environmental","global","legal","relevant","accurate","capable","dangerous","dramatic","efficient","powerful","foreign","hungry","practical","psychological","severe","suitable","numerous","sufficient","unusual","consistent","cultural",
-        "existing","famous","pure","obvious","careful","latter","obviously","unhappy","acceptable","aggressive","distinct","eastern","logical","reasonable","strict","successfully","administrative","automatic","civil","former","massive","southern","unfair","visible","alive","angry","desperate",
-        "exciting","friendly","lucky","realistic","sorry","ugly","unlikely","anxious","comprehensive","curious","impressive","informal","inner","pleasant","insufferable",
+    private static $adjectives = array(
+        "agreeable","amused","ancient","angry","annoyed","anxious","arrogant","ashamed","average","awful","bad","beautiful","better","big","bitter","black","blue","boiling","brave","breezy","brief","bright","broad","broken","bumpy","calm","charming","cheerful","chilly","clumsy","cold","colossal",
+        "combative","comfortable","confused","cooing","cool","cooperative","courageous","crazy","creepy","cruel","cuddly","curly","curved","damp","dangerous","deafening","deep","defeated","defiant","delicious","delightful","depressed","determined","dirty","disgusted","disturbed","dizzy","dry","dull","dusty",
+        "eager","early","elated","embarrassed","empty","encouraging","energetic","enthusiastic","envious","evil","excited","exuberant","faint","fair","faithful","fantastic","fast","fat","few","fierce","filthy","fine","flaky","flat","fluffy","foolish","frail","frantic","fresh","friendly","frightened","funny",
+        "fuzzy","gentle","giant","gigantic","good","gorgeous","greasy","great","green","grieving","grubby","grumpy","handsome","happy","hard","harsh","healthy","heavy","helpful","helpless","high","hilarious","hissing","hollow","homeless","horrible","hot","huge","hungry","hurt","hushed","husky","icy","ill",
+        "immense","itchy","jealous","jittery","jolly","juicy","kind","large","late","lazy","light","little","lively","lonely","long","loose","loud","lovely","low","lucky","magnificent","mammoth","many","massive","melodic","melted","mighty","miniature","moaning","modern","mute","mysterious","narrow","nasty",
+        "naughty","nervous","new","nice","nosy","numerous","nutty","obedient","obnoxious","odd","old","orange","ordinary","outrageous","panicky","perfect","petite","pleasant","precious","pretty","prickly","proud","puny","purple","purring","quaint","quick","quickest","quiet","rainy","rapid","rare","raspy",
+        "ratty","red","relieved","repulsive","resonant","ripe","roasted","robust","rotten","rough","round","sad","salty","scary","scattered","scrawny","screeching","selfish","shaggy","shaky","shallow","sharp","shivering","short","shrill","silent","silky","silly","skinny","slimy","slippery","slow","small",
+        "smiling","smooth","soft","solid","sore","sour","spicy","splendid","spotty","square","squealing","stale","steady","steep","sticky","stingy","straight","strange","striped","strong","successful","sweet","swiftly","tall","tame","tan","tart","tasteless","tasty","tender","tender","tense","terrible",
+        "testy","thirsty","thoughtful","thoughtless","thundering","tight","tiny","tired","tough","tricky","troubled","ugliest","ugly","uneven","upset","uptight","vast","victorious","vivacious","voiceless","wasteful","watery","weak","weary","wet","whispering","wicked","wide","wide-eyed","witty","wonderful",
+        "wooden","worried","yellow","young","different","popular","insufferable","unfortunate"
+    );
         
-        "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth","eleventh","twelfth","thirteenth","fourteenth","fifteenth","sixteenth","seventeenth","eighteenth","nineteenth","twentieth");
+    private static $ordinal = array(    
+        "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth","eleventh","twelfth","thirteenth","fourteenth","fifteenth","sixteenth","seventeenth","eighteenth","nineteenth","twentieth"
+    );
     
     protected static function getRandomNoun($excluded = null) {
         do {
@@ -50,6 +71,12 @@ class NameGenerator
     
     protected static function getRandomAdj() {
         return self::$adjectives[rand(0, count(self::$adjectives) - 1)];
+    }
+    
+    protected static function addAn($word) {
+        return in_array(substr($word, 0, 1), array('a', 'i', 'u', 'e', 'o', 'y'))
+            ? 'an '.$word
+            : 'a '.$word;
     }
     
     /**
@@ -69,7 +96,7 @@ class NameGenerator
         	case 2:
         	    return 'The '.self::getRandomAdj().' '.$second;
     	    case 3:
-    	        return 'A '.self::getRandomAdj().' '.$second;
+    	        return ucfirst(self::addAn(self::getRandomAdj())).' '.$second;
         	case 4:
         	    return 'The '.$first.' in '.$second;
     	    case 5:
