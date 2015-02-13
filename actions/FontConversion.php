5640b3ea06dcc2e56ce92d744c8e9b2c332d6683
antoinerobin <antoine.robin@vesperiagroup.com>
2015-02-13 14:47:08 +0100

    allow x-zip-compressed, x-zip ... to be uploaded

--- actions/FontConversion.php
+++ actions/FontConversion.php
@@ -153,7 +153,7 @@ class FontConversion extends \tao_actions_CommonModule
             $_FILES['content']['type'] = finfo_file($finfo, $_FILES['content']['tmp_name']);
         }
 
-        if (!$_FILES['content']['type'] || $_FILES['content']['type'] !== 'application/zip') {
+        if (!$_FILES['content']['type'] || preg_match('#application\/(x-g?)?zip(-compressed)?#', $_FILES['content']['type']) !== 1) {
             return array('error' => __('Media must be a zip archive, got ' . $_FILES['content']['type']));
         }
 
