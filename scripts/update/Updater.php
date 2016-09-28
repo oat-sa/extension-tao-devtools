e33f52a935de0ccf7c431f568efcda5cf02896bf
Dieter Raber <dieter@taotesting.com>
2016-09-28 08:48:10 +0200

    increased version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -33,6 +33,6 @@ class Updater extends \common_ext_ExtensionUpdater
      */
     public function update($initialVersion)
     {
-        $this->skip('0', '2.14.2');
+        $this->skip('0', '2.15.0');
     }
 }
