9d26c5fe67c0a57a284343f34be3b7c9c57e0b56
Mikhail Kamarouski <Komarouski@1pt.com>
2017-03-09 14:56:25 +0300

    Inproved compatibility with latest persistance interface

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -33,6 +33,6 @@ class Updater extends \common_ext_ExtensionUpdater
      */
     public function update($initialVersion)
     {
-        $this->skip('0', '2.20.1');
+        $this->skip('0', '2.20.2');
     }
 }
