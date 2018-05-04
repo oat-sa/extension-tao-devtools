3d146a3cdbc5f7867db77719a8393024eefe9600
Joel Bout <joel@taotesting.com>
2018-05-04 10:30:55 +0200

    Bump v3.10.0

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -33,6 +33,6 @@ class Updater extends \common_ext_ExtensionUpdater
      */
     public function update($initialVersion)
     {
-        $this->skip('0', '3.9.0');
+        $this->skip('0', '3.10.0');
     }
 }
