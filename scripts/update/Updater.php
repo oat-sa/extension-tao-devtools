a1eb03bd2809a025eb06f3269a0eb7fc22f0597a
alexey <aliakseiv@taotesting.com>
2018-12-13 15:19:40 +0300

    Bump major version after merge

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -33,6 +33,6 @@ class Updater extends \common_ext_ExtensionUpdater
      */
     public function update($initialVersion)
     {
-        $this->skip('0', '4.0.0');
+        $this->skip('0', '5.0.0');
     }
 }
